613c7a137bcdbd7907b1d6e1504fbecd5ae656fe
Tarun walia <taruncs3@gmail.com>
2017-09-09 13:45:51 +0530

    adding testng

--- src/test/java/com/tests/TestDemo.java
+++ src/test/java/com/tests/TestDemo.java
@@ -1,6 +1,14 @@
 package com.tests;
 
+import org.testng.annotations.Test;
+
 public class TestDemo {
+	
+	@Test
+	public void testCheck(){
+		System.out.println("Shnaya walia");
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Tarun walia devOps demo check.");
