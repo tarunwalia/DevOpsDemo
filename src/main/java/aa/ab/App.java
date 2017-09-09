498d59a14d8f6d7e2c8dc6174fecb34dc7a7941e
Tarun walia <taruncs3@gmail.com>
2017-09-09 12:58:37 +0530

    adding devOpsDemo project

---
+++ src/main/java/aa/ab/App.java
@@ -0,0 +1,13 @@
+package aa.ab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tests/TestDemo.java
@@ -0,0 +1,9 @@
+package com.tests;
+
+public class TestDemo {
+	
+	public static void main(String[] args) {
+		System.out.println("Tarun walia devOps demo check.");
+	}
+
+}
